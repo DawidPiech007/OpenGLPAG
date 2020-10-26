2bf46e030edfdf59d62c75d124887eafcb0847a3
piechu007 <534405691+piechu007@users.noreply.github.com>
2020-10-26 18:15:16 +0100

    Bialy kwadrat na czarnym tle (main.cpp)

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,135 @@
 
 #include <GLFW/glfw3.h> // Include glfw3.h after our OpenGL definitions
 
+//==================================== [ TUTORIAL START ] ===========================================//
+#include <glm/glm.hpp>
+
+GLFWwindow* window;
+
+/* Initialize vertices of our triangle */
+glm::vec3 vertices[] = { glm::vec3(-1.0f,  1.0f, 0.0f),
+                         glm::vec3(1.0f, -1.0f, 0.0f),
+                         glm::vec3(-1.0f, -1.0f, 0.0f),
+                         glm::vec3(-1.0f,  1.0f, 0.0f),
+                         glm::vec3(1.0f, -1.0f, 0.0f),
+                         glm::vec3(1.0f, 1.0f, 0.0f)
+};
+
+/* Initialize Vertex Buffer Object */
+GLuint VBO = NULL;
+
+int init(int width, int height)
+{
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(width, height, "Hello Triangle", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+
+    /* Initialize GLEW */       
+    /*                                                          TAK BY£O
+    if (glewInit() != GLEW_OK)                                  TAK BY£O
+        return -1;                                              TAK BY£O
+    */  
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))//  TAK JEST
+        return -1;                                          //  TAK JEST
+
+    /* Set the viewport */
+    glViewport(0, 0, width, height);
+
+    return true;
+
+    /* Set clear color */                                   // Kolor
+    glClearColor(0.0f, 1.0f, 1.0f, 1.0f);                   // Kolor
+}
+
+int loadContent()
+{
+    /* Create new buffer to store our triangle's vertices */
+    glGenBuffers(1, &VBO);
+
+    /* Tell OpenGL to use this buffer and inform that this buffer will contain an array of vertices*/
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    /* Fill buffer with data */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Enable a generic vertex attribute array */
+    glEnableVertexAttribArray(0);
+
+    /* Tell OpenGL how to interpret the data in the buffer */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    return true;
+}
+
+void render(float tpf)
+{
+    /* Clear the color buffer */                  // Kolor (odœwierzenie bufora)
+    //glClear(GL_COLOR_BUFFER_BIT);                 // Kolor 
+
+    glViewport(640/4, 480/4, 640/2, 480/2); // Trójk¹t 2 razy mniejszy na œrodku ekranu
+
+    /* Draw our triangle */
+    glDrawArrays(GL_TRIANGLES, 0, 6);           // Rysuje 2 trójk¹ty 
+}
+
+void update()
+{
+    float oldTime = 0.0f;
+    float newTime = 0.0f;
+    float gameTime = 0.0f;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Update game time value */
+        oldTime = newTime;
+        newTime = (float)glfwGetTime();
+        gameTime = newTime - oldTime;
+
+        /* Render here */
+        render(gameTime);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+}
+
+
+
+int main(void)
+{
+    if (!init(640, 480))
+        return -1;
+
+    if (!loadContent())
+        return -1;
+
+    update();
+
+    glfwTerminate();
+    return 0;
+}
+
+//==================================== [ TUTORIAL  STOP ] ===========================================//
+
+/*
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
@@ -181,3 +310,4 @@ int main(int, char**)
 
     return 0;
 }
+*/
