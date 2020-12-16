02ceabcea44a59d1da96d54cedca33fa0f6043c0
piechu007 <534405691+piechu007@users.noreply.github.com>
2020-12-16 20:44:11 +0100

    Dodałem klasę kamery (jest pusta)
    program się kompiluje

---
+++ src/MyCamera.hpp
@@ -0,0 +1,20 @@
+#ifndef MY_CAMERA
+#define MY_CAMERA
+
+#include <glm/mat4x4.hpp>
+//#include "GraphNode.hpp"
+#include <GLFW/glfw3.h>
+
+
+class SceneRoot
+{
+public:
+
+
+private:
+
+	glm::vec3 position;
+
+};
+
+#endif
